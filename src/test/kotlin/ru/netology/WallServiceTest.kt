d064c2ec0e7794a42af870f2530fd1b1be3a6336
Alexey Mileev <ano.blagodat@yandex.ru>
2024-08-14 17:23:30 +0300

    change WallServiceTest

--- src/test/kotlin/ru/netology/WallServiceTest.kt
+++ src/test/kotlin/ru/netology/WallServiceTest.kt
@@ -1,50 +1,15 @@
-//package ru.netology
-//
-//import org.junit.Test
-//import org.junit.Assert.*
-//import org.junit.Before
-//
-//class WallServiceTest {
-//
-//    @Before
-//    fun clearBeforeTest() {
-//        WallService.clear()
-//    }
-//
-//    @Test
-//    fun addPost() {
-//        val post = Post(0, "text")
-//        val addedPost = WallService.add(post)
-//        assertNotEquals(0, addedPost.id)
-//    }
-//
-//    @Test
-//    fun updatePost() {
-//        val post = Post(0, "111")
-//        WallService.add(post)
-//        val updatedPost = Post(1, "222")
-//        val result = WallService.update(updatedPost)
-//        assertTrue(result)
-//    }
-//
-//    @Test
-//    fun notUpdatePost() {
-//        val post = Post(0, "111")
-//        WallService.add(post)
-//        val updatedPost = Post(20, "222")
-//        val result = WallService.update(updatedPost)
-//        assertFalse(result)
-//    }
-//
-//
-//}
 package ru.netology
 
 import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Before
 
 class WallServiceTest {
+    @Before
+   fun clearBeforeTest() {
+      WallService.clear()
+   }
+
 
     @Test
     fun updateExisting() {
